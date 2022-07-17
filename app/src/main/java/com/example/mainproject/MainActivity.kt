349103857716834756f99ee5484d98e22dc280c8
Robert Gevorgyan <robertgevorgyan82@gmail.com>
2022-07-17 14:45:45 +0400

    done bottom navigation with navigation component

--- app/src/main/java/com/example/mainproject/MainActivity.kt
+++ app/src/main/java/com/example/mainproject/MainActivity.kt
@@ -1,59 +1,31 @@
 package com.example.mainproject
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.blue
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.mainproject.Fragment.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNav: BottomNavigationView
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         initView()
-        setMenuListener()
-    }
+        bottomNav.setupWithNavController(navController)
 
-    private fun setMenuListener() {
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home_nav_bt -> {
-                    goToSelectedFragment(HomeFragment())
-                    true
-                }
-                R.id.search_nav_bt -> {
-                    goToSelectedFragment(SearchFragment())
-                    true
-                }
-                R.id.cart_nav_bt -> {
-                    goToSelectedFragment(CartFragment())
-                    true
-                }
-                R.id.profile_nav_bt -> {
-                    goToSelectedFragment(ProfileFragment())
-                    true
-                }
-                R.id.more_nav_bt -> {
-                    goToSelectedFragment(MoreFragment())
-                    true
-                }
-                else -> {
-                    true
-                }
-            }
-        }
-    }
-
-
-    private fun goToSelectedFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, fragment, fragment.javaClass.simpleName)
-            .commit()
     }
 
     private fun initView() {
         bottomNav = findViewById(R.id.bottom_nav)
+        navController = findNavController(R.id.container)
     }
 }
